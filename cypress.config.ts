b355433e5990bee75d37b8307136988dba8eb9c5
AyobomiOmojola <ayomojola12@gmail.com>
2024-08-30 13:24:53 +0100

    made some changes

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,10 +2,12 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    reporter: 'mochawesome',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
   viewportWidth: 1440,
   viewportHeight: 1000,
+  screenshotOnRunFailure: true
 });
